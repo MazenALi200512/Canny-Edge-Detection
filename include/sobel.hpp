1384b6ece596bd437a6b28f23d95a0369786030c
Your Name <you@example.com>
2026-06-14 19:21:19 +0300

    Sobel

---
+++ include/sobel.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "image.hpp"
+#include <vector>
+#include <cstdint>
+
+struct Gradient
+{
+    std::vector<int16_t> gx;
+    std::vector<int16_t> gy;
+
+    int width;
+    int height;
+
+    Gradient(int w, int h)
+        : gx(w*h),
+          gy(w*h),
+          width(w),
+          height(h)
+    {
+    }
+};
+
+Gradient sobel(const Image& input);
