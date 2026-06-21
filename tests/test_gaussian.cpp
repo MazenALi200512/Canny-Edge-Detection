377343cb47bcea8fdba389a913e80bba89cb163c
Mazen <99512078+MazenALi200512@users.noreply.github.com>
2026-06-21 19:35:20 +0300

    Update test_gaussian.cpp

--- tests/test_gaussian.cpp
+++ tests/test_gaussian.cpp
@@ -7,7 +7,7 @@ TEST(Gaussian, UniformImage)
     Image img(100, 75);
     for(int i = 0; i < img.width * img.height; i++)
         img.data[i] = 128;
-    Image out = gaussianBlur(img);
+    Image out = gaussianBlurSeparable(img);
     for(int y = 2; y < 73; y++)
         for(int x = 2; x < 98; x++)
             EXPECT_NEAR(out.at(x, y), 128, 1);
@@ -18,7 +18,7 @@ TEST(Gaussian, BlackImage)
     Image img(100, 75);
     for(int i = 0; i < img.width * img.height; i++)
         img.data[i] = 0;
-    Image out = gaussianBlur(img);
+    Image out = gaussianBlurSeparable(img);
     for(int i = 0; i < out.width * out.height; i++)
         EXPECT_EQ(out.data[i], 0);
 }
@@ -27,7 +27,7 @@ TEST(Gaussian, ImpulseResponse)
 {
     Image img(21, 21);
     img.at(10, 10) = 255;
-    Image out = gaussianBlur(img);
+    Image out = gaussianBlurSeparable(img);
     EXPECT_GT(out.at(10, 10), 0);
     EXPECT_LT(out.at(10, 10), 255);
     EXPECT_EQ(out.at(9, 10), out.at(11, 10));
