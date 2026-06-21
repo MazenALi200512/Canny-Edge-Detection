5c65af491ebc176c1e49e4e774d0c9346f81fba9
Mazen <99512078+MazenALi200512@users.noreply.github.com>
2026-06-21 19:38:23 +0300

    Update test_magnitude.cpp

--- tests/test_magnitude.cpp
+++ tests/test_magnitude.cpp
@@ -11,8 +11,10 @@ TEST(Magnitude, L1AndL2NonZero)
         g.gx[i] = 50;
         g.gy[i] = 20;
     }
-    Image l1 = magnitudeL1(g);
-    Image l2 = magnitudeL2(g);
+    Image l1(8,8);
+    Image l2(8,8);
+    magnitudeL1_inplace(g,l1);
+    magnitudeL2_inplace(g,l2);
     EXPECT_GT(l1.at(0, 0), 0);
     EXPECT_GT(l2.at(0, 0), 0);
 }
@@ -20,8 +22,10 @@ TEST(Magnitude, L1AndL2NonZero)
 TEST(Magnitude, ZeroGradient)
 {
     Gradient g(100, 75);
-    Image l1 = magnitudeL1(g);
-    Image l2 = magnitudeL2(g);
+    Image l1(100,75);
+    Image l2(100,75);
+    magnitudeL1_inplace(g,l1);
+    magnitudeL2_inplace(g,l2);
     for(int i = 0; i < l1.width * l1.height; i++)
         EXPECT_EQ(l1.data[i], 0);
     for(int i = 0; i < l2.width * l2.height; i++)
@@ -33,8 +37,10 @@ TEST(Magnitude, Saturation)
     Gradient g(1, 1);
     g.gx[0] = 1000;
     g.gy[0] = 1000;
-    Image l1 = magnitudeL1(g);
-    Image l2 = magnitudeL2(g);
+    Image l1(1,1);
+    Image l2(1,1);
+    magnitudeL1_inplace(g,l1);
+    magnitudeL2_inplace(g,l2);
     EXPECT_EQ(l1.at(0, 0), 255);
     EXPECT_EQ(l2.at(0, 0), 255);
 }
